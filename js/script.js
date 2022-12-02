b0a00a2b81400c4f1324534b1413f2b16601cfe2
Jimmy Huang <mayday.jimmy@gmail.com>
2022-12-02 18:32:40 -0500

    added api all api keys and formatted HTML

--- js/script.js
+++ js/script.js
@@ -12,8 +12,30 @@ const key = "&api_key=5e9bcac9-4573-44c1-892a-cd7da4fae82d"; // API Key
 
 
 // Cache DOM Element Reference ---------------------------------------------
-const $dpt = $('#dpt')
-const $arr = $('#arr')
+const $flight = $('#flight')
+const $fltcode = $('#fltcode')
+const $status = $('#status')
+
+const $dptapt = $('#dptapt')
+const $arrapt = $('#arrapt')
+
+const $dptgate = $('#dptgate')
+const $arrgate = $('#arrgate')
+
+const $schdpt = $('#schdpt')
+const $scharr = $('#scharr')
+
+const $accdpt = $('#accdpt')
+const $accarr = $('#accarr')
+
+const $estdpt = $('#estdpt')
+const $estarr = $('#estarr')
+
+const $aircraft = $('#aircraft')
+const $speed = $('#speed')
+const $altitude = $('#altitude')
+const $heading = $('#heading')
+
 const $form = $('form') // Form is a section with the input boxes
 const $airline = $('#airline')
 const $fltnum = $('#fltnum')
@@ -57,6 +79,7 @@ function getData(fullURL) {
     $.ajax(fullURL).then( ( data ) => {
 
         // if successful ajax, calls displayData function
+        console.log("displays")
         displayData(data)
 
     }, ( error ) => {
@@ -71,19 +94,50 @@ function displayData(data) {
 
     // Returns error if invalid flight is inputted
     if ('error' in data === true) { // if "error" key is in the "data" object
-        $dpt.text("Flight Not Found")
-        $arr.text("Flight Not Found")
+        $dptapt.text("Flight Not Found")
+        $arrapt.text("Flight Not Found")
     }
 
     else {
     // Display flight information
         console.log("ajax is working")
-    
-        // show departure airport name
-        $dpt.text(data.response.dep_name)
 
-        // show departure airport name
-        $arr.text(data.response.arr_name)
+        // show airline name and flight number
+        $flight.text(data.response.airline_name + " " + data.response.flight_number)
+
+        // show flight iata/icao number
+        $fltcode.text(`(${data.response.flight_icao}/${data.response.flight_iata})`)
+
+        // show flight status
+        // Capitalize the first letter of the status //
+
+        $status.text(data.response.status)
+    
+        // show dep airport and arr airport details
+        console.log(data.response.dep_icao)
+
+        $dptapt.text(`${data.response.dep_name} (${data.response.dep_icao}) - ${data.response.dep_city}, ${data.response.dep_country}`)
+        $arrapt.text(`---------> ${data.response.arr_name} (${data.response.arr_icao}) - ${data.response.arr_city}, ${data.response.arr_country}`)
+
+        // show dep and arr gate/terminal details
+        $dptgate.text(`${data.response.dep_gate} (Terminal ${data.response.dep_terminal})`)
+        $arrgate.text(`${data.response.arr_gate} (Terminal ${data.response.arr_terminal})`)
+
+        // show times
+        $schdpt.text(data.response.dep_time)
+        $scharr.text(data.response.arr_time)
+        $estdpt.text(data.response.dep_estimated)
+        $estarr.text(data.response.arr_estimated)
+        $accdpt.text(data.response.dep_actual)
+        $accarr.text(data.response.arr_actual)
+
+        // show current aircraft stats
+        $aircraft.text(`${data.response.aircraft_icao}`)
+        $speed.text(`${data.response.speed} kph`)
+        $altitude.text(`${data.response.alt} ft`)
+        $heading.text(`${data.response.dir} deg`)
+        // 
+
     }
 }
 // -------------------------------------------------------------
